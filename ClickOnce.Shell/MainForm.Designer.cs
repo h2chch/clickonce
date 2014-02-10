6d988e3f71aba4e8b43386cce4ae7db448ae2ab3
Hendra Hidayat <hhidayat@gmail.com>
2014-02-10 18:46:24 +0800

    initial version

---
+++ ClickOnce.Shell/MainForm.Designer.cs
@@ -0,0 +1,62 @@
+﻿namespace ClickOnce.Shell
+{
+    partial class MainForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.labelTitle = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // labelTitle
+            // 
+            this.labelTitle.AutoSize = true;
+            this.labelTitle.Location = new System.Drawing.Point(58, 34);
+            this.labelTitle.Name = "labelTitle";
+            this.labelTitle.Size = new System.Drawing.Size(47, 13);
+            this.labelTitle.TabIndex = 0;
+            this.labelTitle.Text = "label100";
+            // 
+            // MainForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(284, 262);
+            this.Controls.Add(this.labelTitle);
+            this.Name = "MainForm";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.MainForm_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label labelTitle;
+    }
+}
+
---
+++ ClickOnce.Shell/MainForm.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using ClickOnce.Lib;
+using System.Deployment.Application;
+using ClickOnce.Shell.Properties;
+
+namespace ClickOnce.Shell
+{
+    public partial class MainForm : Form
+    {
+        public MainForm()
+        {
+            InitializeComponent();
+        }
+
+        private void MainForm_Load(object sender, EventArgs e)
+        {
+            MessageBox.Show("Is Network Deployed: " + ApplicationDeployment.IsNetworkDeployed);
+            MessageBox.Show("Is First Run: " + ApplicationDeployment.CurrentDeployment.IsFirstRun);
+
+            if (ApplicationDeployment.IsNetworkDeployed && ApplicationDeployment.CurrentDeployment.IsFirstRun)
+            {
+                Settings.Default.IsFirstRun = false;    
+                Settings.Default.Save(); 
+            }
+
+
+
+
+        }
+    }
+}
